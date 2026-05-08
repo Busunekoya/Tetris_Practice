97a4687048a1427967e9e300b9462cea61ae9ed0
Busunekoya <megerubusuneko@gmail.com>
2026-05-08 12:33:18 +0900

    エリア生成

---
+++ Assets/SetTiles.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.UI;
+using UnityEngine;
+
+public class SetTiles : MonoBehaviour
+{
+    /// <summary>
+    /// タイルのプレハブ
+    /// </summary>
+    [SerializeField] private GameObject tilePrefab;
+    private Transform ariaObject;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //ariaObjectにこのゲームオブジェクトのTransformを代入
+        ariaObject = this.gameObject.transform;
+        //タイルを10x20の範囲で生成
+        for(int x = 0; x < 10; x++)
+        {
+            for(int y = 0; y < 20; y++)
+            {
+                GameObject tile = Instantiate(tilePrefab, ariaObject.TransformPoint(-5+x, -10+y, 0), Quaternion.identity);
+                tile.transform.parent = ariaObject;
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
