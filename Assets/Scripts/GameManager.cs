47ea5b3ee7ecef41b037d46587e965dfb840f547
Busunekoya <megerubusuneko@gmail.com>
2026-05-08 13:11:22 +0900

    ゲームマネージャー作成

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public GameObject[] MinoPrefabs;
+    private GameObject CurrentMino;
+    private GameObject NextMino;
+    private GameObject HoldMino;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
