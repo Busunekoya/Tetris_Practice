2f1493c26c5b92e9f54870e5ab8898d986b46f71
Busunekoya <megerubusuneko@gmail.com>
2026-07-08 11:01:09 +0900

    Update GameManager.cs

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -4,33 +4,97 @@ using UnityEngine;
 //参考: https://masavlog.com/programming/tetris/unity-tetris-3/
 public class GameManager : MonoBehaviour
 {
+    /// <summary>
+    /// テトリスのミノを入れる配列
+    /// </summary>
     public GameObject[] MinoPrefabs = new GameObject[7];
+    /// <summary>
+    /// 動かすミノ
+    /// </summary>
     private GameObject currentMino;
+    /// <summary>
+    /// currentMinoについているMinoComponent
+    /// </summary>
     private Mino currentMinoComponent;
+    /// <summary>
+    /// 次のミノ
+    /// </summary>
     private GameObject nextMino;
+    /// <summary>
+    /// ホールド中のミノ
+    /// </summary>
     private GameObject holdMino = null;
+    /// <summary>
+    /// 次のミノを表示する場所
+    /// </summary>
     public Transform NextMinoPos;
+    /// <summary>
+    /// ホールド中のミノを表示する場所
+    /// </summary>
     public Transform HoldMinoPos;
+    /// <summary>
+    /// ゲーム中か
+    /// </summary>
     public bool playing = true;
+    /// <summary>
+    /// 保持可能か
+    /// </summary>
     private bool holdable = true;
+    /// <summary>
+    /// 落ちるまでの時間設定
+    /// </summary>
     private float rawfallTime = 1f;
+    /// <summary>
+    /// 下降中に落ちるまでの時間設定
+    /// </summary>
     private float rawfalldownTime = 0.1f;
+    /// <summary>
+    /// 落ちるまでの時間
+    /// </summary>
     private float fallTime = 1f;
+    /// <summary>
+    /// 落ちるまでのタイマー
+    /// </summary>
     private float fallTimer = 0f;
+    /// <summary>
+    /// 初期位置
+    /// </summary>
     private Vector2Int firstTilePos = new Vector2Int(5, 18);
+    /// <summary>
+    /// 現在の位置
+    /// </summary>
     private Vector2Int currentMinoPos;
+    /// <summary>
+    /// タイルのプログラム
+    /// </summary>
     public SetTiles setTiles;
+    /// <summary>
+    /// スコアマネージャー
+    /// </summary>
     public GameScoreManager gameScoreManager;
+    /// <summary>
+    /// 落ち切ったミノを保存するTransform
+    /// </summary>
     [SerializeField]private Transform FallenMinos;
+    /// <summary>
+    /// 1. ゲームスコアマネージャーの存在を確認する
+    /// 2. nextMinoを設定する
+    /// </summary>
     void Awake()
     {
         if(gameScoreManager == null)Debug.LogError("GameScoreManager not found");
         nextMino = Instantiate(MinoPrefabs[Random.Range(0, MinoPrefabs.Length)], NextMinoPos);
     }
+    /// <summary>
+    /// SetNextMinoメソッドを動かす
+    /// </summary>
     void Start()
     {
         SetNextMino();
     }
+    /// <summary>
+    /// もし、playing=trueならMinoMovement、MinoRotation、MinoFallメソッドを動かす
+    /// </summary>
     void Update()
     {
         if(!playing)return;
@@ -39,42 +103,61 @@ public class GameManager : MonoBehaviour
         MinoFall();
     }
     /// <summary>
-    /// 次のミノを設定する
+    /// currentMinoがnullであることを確認し、SetCurrentMinoメソッドを動かす
+    /// nextMinoはMinoPrefabsの中からランダムに選択されるようにし、座標はNextMinoPosにする
+    /// nextMinoのMinoコンポーネントからSetTransformPosメソッドを動かし、NextMinoPos
     /// </summary>
     void SetNextMino()
     {
+        //currentMinoがnullであることを確認
         if(currentMino != null)return;
+        //SetCurrentMinoメソッドを動かす
         SetCurrentMino(nextMino);
+        //nextMinoはMinoPrefabsの中からランダムに選択されるようにし、親はNextMinoPosにする
         nextMino = Instantiate(MinoPrefabs[Random.Range(0, MinoPrefabs.Length)], NextMinoPos);
+        //nextMinoのMinoコンポーネントからSetTransformPosメソッドを動かし、NextMinoPosの座標を基準にミノを設定する
         nextMino.GetComponent<Mino>().SetTransformPos(NextMinoPos);
     }
     void SetCurrentMino(GameObject mino)
     {
+        //currentMinoを引数のminoとする
         currentMino = mino;
+        //currentMinoComponentをcurrentMinoに再設定する
         currentMinoComponent = currentMino.GetComponent<Mino>();
+        //currentMinoの親を外す
         currentMino.transform.parent = null;
+        //currentMinoの座標を初期位置とする
         currentMinoPos = firstTilePos;
+        //MinoComponentより、SetChildPosコンポーネントを動かし、setTilesのfirstTilePosの位置を基準とする
         currentMinoComponent.SetChildPos(firstTilePos, setTiles);
-
+        //もし動かせないのなら、ゲームオーバーとする
         if (!isMoveAble(Vector2Int.zero))
         {
             GameOver();
         }
     }
+    /// <summary>
+    /// ゲームオーバー時の処理
+    /// </summary>
     void GameOver()
     {
+        //playingをfalseとする
         playing = false;
         Debug.Log("Game Over");
     }
+    //ミノを交代する
     void SwitchMino()
     {
+        //holdableがtrueであり、currentMinoがnullではないことを確認する
         if(!holdable || currentMino == null) return;
+        //holdableをfalseにする(何回も保持を変更することを防ぐため)
         holdable = false;
+        //holdMinoとcurrentMinoを入れ替える
         GameObject tempMino = holdMino;
         holdMino = currentMino;
         holdMino.GetComponent<Mino>().SetTransformPos(HoldMinoPos);
-
         currentMino = tempMino;
+        //currentMinoがあるのなら、SetCurrentMinoで再設定し、ないのなら次のミノを設定する
         if(currentMino != null)
         {
             SetCurrentMino(currentMino);
@@ -89,6 +172,7 @@ public class GameManager : MonoBehaviour
     /// </summary>
     void MinoMovement()
     {
+        //左右キーが押された時、それぞれ左右に1マスずつ動かす。ただし、どのブロックもはみ出さないようにする
         if (Input.GetKeyDown(KeyCode.LeftArrow))
         {
             isMoveAble(new Vector2Int(-1,0));
@@ -96,6 +180,7 @@ public class GameManager : MonoBehaviour
         {
             isMoveAble(new Vector2Int(1, 0));
         }
+        //下キーが押されているかどうかでそれぞれ対応する値に設定する
         if (Input.GetKey(KeyCode.DownArrow))
         {
             fallTime = rawfalldownTime * Mathf.Pow(0.9f, gameScoreManager.level - 1);
@@ -104,6 +189,7 @@ public class GameManager : MonoBehaviour
         {
             fallTime = rawfallTime * Mathf.Pow(0.9f, gameScoreManager.level - 1);
         }
+        //スペースキーが押された時、保持ミノを入れ替える
         if(Input.GetKeyDown(KeyCode.Space))
         {
             SwitchMino();
@@ -114,6 +200,7 @@ public class GameManager : MonoBehaviour
     /// </summary>
     void MinoRotation()
     {
+        //上キーが押された時、-90°回転させる、ただし、はみ出すようであれば回転させない
         if (Input.GetKeyDown(KeyCode.UpArrow))
         {
             isRotateAble(-90);
@@ -124,12 +211,15 @@ public class GameManager : MonoBehaviour
     /// </summary>
     void MinoFall()
     {
+        //プレイ中であることを確認する
         if(!playing)return;
-
+        //もし、fallTimerの値がfallTimeよりも大きいならば、下方向に移動する
         if(fallTimer >= fallTime)
         {
+            //下方向に移動する
             if(isMoveAble(new Vector2Int(0, -1)))
             {
+                //もし、下キーが押されているのなら加点する
                 if (Input.GetKey(KeyCode.DownArrow))
                 {
                     gameScoreManager.AddScore(10);
@@ -137,6 +227,7 @@ public class GameManager : MonoBehaviour
             }
             else
             {
+                //ミノをTileに追加するメソッドを動かす
                 AddToTile(currentMino);
                 CheckLines();
                 //ミノが落ちきったときの処理
